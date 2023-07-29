8d8fe12b61b48826ecf85f7a7bcb2b05b6ba861f
minhthtran <tranminhthu9aa@gmail.com>
2023-07-29 15:03:10 +0700

    dangkygv-dangkydn

--- header_footer/footer.php
+++ header_footer/footer.php
@@ -1,14 +1,14 @@
 <footer>
    <div class="group_info">
       <div class = "group_row">
-         <img src="~/../asset/logoUFM.png" alt="Your Image">
+         <img src="../../asset/logoUFM.png" alt="Your Image">
          <div>
             <label id="title">TRƯỜNG ĐẠI HỌC TÀI CHÍNH - MARKETING </label><br>
             <label id="content_title">KHOA CÔNG NGHỆ THÔNG TIN </label>
          </div>
       </div>
       <div class = "group_row">
-         <img src="~/../asset/logoKNTT.png" alt="Your Image">
+         <img src="../../asset/logoKNTT.png" alt="Your Image">
          <div>
             <label id="title">KẾT NỐI THỰC TẬP GIỮA SINH VIÊN</label><br>
             <label id="content_title">VÀ DOANH NGHIỆP IT </label>
--- header_footer/header.php
+++ header_footer/header.php
@@ -3,27 +3,29 @@
 <!DOCTYPE html> 
 <html>
 <head>
-    <title>Trang chủ</title>
-    <link href="~/../css/style_khoa.css" rel="stylesheet">
+    <link href="../../css/style_khoa.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="../../css/dangkygiangvien.css" rel="stylesheet">
+    <link href="../../css/dangkydoanhnghiep.css" rel="stylesheet">
 </head>
 <body>
     <header>
         <div id ="header_top">
             <div id="header_left">
-                <img src="~/../asset/logoKNTT.png" alt="Your Image">
+                <img src="../../asset/logoKNTT.png" alt="Your Image">
                 <label>Kết nối thực tập</label>
             </div>
             <div class="header_right">
-                <img src="~/../asset/user.png" alt="Your Image">
+                <img src="../../asset/user.png" alt="Your Image">
                 <label>Đăng nhập</label>
             </div>
             <div class="header_right">
-                <img src="~/../asset/gmail.png" alt="Your Image">
+                <img src="../../asset/gmail.png" alt="Your Image">
                 <label>Liên hệ admin</label>
             </div>
         </div>
         <div id = "menu">
-            <a href="#"> <img src="~/../asset/icon_home.png" alt="Your Image"> </a>
+            <a href="#"> <img src="../../asset/icon_home.png" alt="Your Image"> </a>
             <a href="#">Giới thiệu</a>
             <a href="#">Thông báo</a>
             <a href="#">Phân công giảng viên</a>
